f1591adde8f6a00a25dadbd1cba1fee15a5c7683
camit2354 <camit2354@gmail.com>
2021-11-08 21:28:28 +0530

    "sns assign 2 commit new"

--- SNS/assign2_v4/Q2/BT18CSE063_AC_C_Sg.py
+++ SNS/assign2_v4/Q2/BT18CSE063_AC_C_Sg.py
@@ -70,14 +70,70 @@ def padding(x, sz):
     return x
 
 # input : any size output : 64 bit
+# Hexadecimal to binary conversion
+
+
+def hex2bin(s):
+    mp = {'0': "0000",
+          '1': "0001",
+          '2': "0010",
+          '3': "0011",
+          '4': "0100",
+          '5': "0101",
+          '6': "0110",
+          '7': "0111",
+          '8': "1000",
+          '9': "1001",
+          'A': "1010",
+          'B': "1011",
+          'C': "1100",
+          'D': "1101",
+          'E': "1110",
+          'F': "1111"}
+    bin = ""
+    for i in range(len(s)):
+        bin = bin + mp[s[i]]
+    return bin
+
+# Binary to hexadecimal conversion
+
+
+def bin2hex(s):
+    mp = {"0000": '0',
+          "0001": '1',
+          "0010": '2',
+          "0011": '3',
+          "0100": '4',
+          "0101": '5',
+          "0110": '6',
+          "0111": '7',
+          "1000": '8',
+          "1001": '9',
+          "1010": 'A',
+          "1011": 'B',
+          "1100": 'C',
+          "1101": 'D',
+          "1110": 'E',
+          "1111": 'F'}
+    hex = ""
+    for i in range(0, len(s), 4):
+        ch = ""
+        ch = ch + s[i]
+        ch = ch + s[i + 1]
+        ch = ch + s[i + 2]
+        ch = ch + s[i + 3]
+        hex = hex + mp[ch]
+
+    return hex
 
 
 def hash(x):
+    # print("hash input : "+bin2hex(x))
     rollno = 63
     rollno = padding(dec2bin(rollno), hashSize)
     ret = rollno
 
-    temp = int(len(x) / hashSize) + 3
+    temp = int(len(x) / hashSize)+3
     x = padding(x, hashSize*temp)
 
     start = 0
